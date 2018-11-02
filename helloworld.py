cae9d953a333c14da9ebf21a2d88aa453e038c3d
sawada48 <32599322+sawada48@users.noreply.github.com>
2018-11-02 05:41:44 +0000

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -976,16 +976,15 @@ def clientBot(op):
 								client.sendMessage(to, "Group ID : {}".format(group.id))
 						elif cmd == "grouplist":
 							if msg._from in admin:
-								if msg.toType == 2:
-									groups = client.getGroupIdsJoined()
-									ret_ = "╔══[ Group List ]"
-									no = 0
-									for gid in groups:
-										group = client.getGroup(gid)
-										no += 1
-										ret_ += "\n╠ {}. {} | {}".format(str(no), str(group.name), str(len(group.members), str(len(group.id)))
-									ret_ += "\n╚══[ Total {} Groups ]".format(str(len(groups)))
-									client.sendMessage(to, str(ret_))
+								groups = client.getGroupIdsJoined()
+								ret_ = "╔══[ Group List ]"
+								no = 0
+								for gid in groups:
+									group = client.getGroup(gid)
+									no += 1
+									ret_ += "\n╠ {}. {} | {}".format(str(no), str(group.name), str(len(group.members)))
+								ret_ += "\n╚══[ Total {} Groups ]".format(str(len(groups)))
+								client.sendMessage(to, str(ret_))
 						elif cmd == "memberlist":
 							if msg.toType == 2:
 								group = client.getGroup(to)
